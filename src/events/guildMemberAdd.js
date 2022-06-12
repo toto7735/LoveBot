4e8caa7ec5c691c5b56ed622b8e52de934e6b65b
toto7735 <totoserver@naver.com>
2022-06-13 01:12:54 +0900

    better

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -11,7 +11,7 @@ module.exports = {
 		.addFields(
 			{ name: 'Participation Prerequisites', value: '1. Able to record Minecraft video in 1080p quality or higher with OBS or other recording programs.\n2. Never use profanity and inappropriate remarks.\n3. Read all of this.' },
 			{ name: 'Preferential Treatment', value: '1. Can use the mic on Discord (Many recordings will be done with people who can use a microphone)' },
-			{ name: 'To Apply', value: '1. Click Apply Button\n2. Upload a Minecraft video (5 seconds to 1 minute) to YouTube, including a scene where the recording starts and stops.\n3. Paste the vide\'s link here. (Only link works!)' },
+			{ name: 'To Apply', value: '1. Click Apply Button\n2. Upload a Minecraft video (5 seconds to 1 minute) to YouTube, including your in game name. (ex: type your in game name in chat)\n3. Paste the video\'s link here. (Only link works!)' },
 		)
 		.setTimestamp()
 		.setFooter({ text: 'Love.', iconURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5BsVtEkRjOvQJdL6SOO_FkBbCBcSVrUB4QA&usqp=CAU' });
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'ready',
     async execute (client) {
-        console.log('I\'m ready to love my fans!!');
+        console.log('I\'m ready to love my fans!! (1)');
     },
 };
